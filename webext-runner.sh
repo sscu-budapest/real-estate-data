fb166c4a1b453542fb62b6b0d46773126e0b86c2
Leo Dumbarton <leo@dumbartonserum.com>
2024-08-08 00:12:30 +0200

    exit if fail

--- webext-runner.sh
+++ webext-runner.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 git pull
-python3 -c "from src.ingatlan_webext import collect;collect()"
+python3 -c "from src.ingatlan_webext import collect;collect()" || exit 1
 git add report.md 
 git commit -m "webext run"
 git push
